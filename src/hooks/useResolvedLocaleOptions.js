452fcc54c195ae5b35f109ffd6ac0394ff08501f
Hendrik de Graaf <hendrik@dhis2.org>
2022-06-12 22:14:06 +0200

    chore: give use-resolved-locale-options an error state

--- src/hooks/useResolvedLocaleOptions.js
+++ src/hooks/useResolvedLocaleOptions.js
@@ -8,21 +8,29 @@ export const useResolvedLocaleOptions = ({
     timeZone,
 }) =>
     useMemo(() => {
-        /**
-         * If no options are provided this will use the values of the user browser
-         * If a locale identifier is provided it will use that to populate options
-         * If any options are provided these will override the options for the
-         * specified locale identifier or browser settings
-         */
-        const { locale: resolvedLocale, ...resolvedOptions } =
-            new Intl.DateTimeFormat(locale, {
-                calendar,
-                timeZone,
-                numberingSystem,
-            }).resolvedOptions()
+        try {
+            /**
+             * If no options are provided this will use the values of the user browser
+             * If a locale identifier is provided it will use that to populate options
+             * If any options are provided these will override the options for the
+             * specified locale identifier or browser settings
+             */
+            const { locale: resolvedLocale, ...resolvedOptions } =
+                new Intl.DateTimeFormat(locale, {
+                    calendar,
+                    timeZone,
+                    numberingSystem,
+                }).resolvedOptions()
 
-        return {
-            resolvedLocale,
-            resolvedOptions,
+            return {
+                resolvedLocale,
+                resolvedOptions,
+            }
+        } catch (error) {
+            return {
+                resolvedLocale: undefined,
+                resolvedOptions: {},
+                error,
+            }
         }
     }, [calendar, locale, numberingSystem, timeZone])
